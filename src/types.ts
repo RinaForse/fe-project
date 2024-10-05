ad48b40be6d8ed8a89f1ebe10ea21e3c5f227352
MussaOPS1 <musa011999@gmail.com>
2024-10-05 15:38:02 +0500

    init

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export interface User {
+    id: string,
+    username: string,
+    fullName: string,
+    email: string,
+    phone: string,
+    address: string,
+    password: string,
+    created: Date,
+    updated: Date
+}
